21553f5d38153526ac250833a1209a0607970017
bayuirfan52 <bayuirfan52@gmail.com>
2020-05-01 22:16:56 +0700

    preparing unit test

---
+++ test/test/CHUnitTest.java
@@ -0,0 +1,23 @@
+package test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import kimia.ui.Main;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author BAYU IRFAN
+ */
+public class CHUnitTest {
+    Main main;
+    
+    @Test
+    public void testRunCH(){
+        
+    }
+}
